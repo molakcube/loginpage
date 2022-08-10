225af0be5650c9165503dff1ad76523fa5100954
JunHoe Heo <jhjjang0902@gmail.com>
2022-08-11 01:17:22 +0900

    로그인 했을 때 id와 pw검사 추가

--- src/signin.js
+++ src/signin.js
@@ -2,15 +2,29 @@ const signIn = document.querySelector(".signinForm");
 const id = document.querySelector(".id input");
 const pw = document.querySelector(".pw input");
 
-function  createId (event) {
+function  createId(event) {
     event.preventDefault();
     const idValue = id.value;
     const pwValue = pw.value;
-    localStorage.setItem(idValue, pwValue);
-    successCreate ();
+    inspectValue(idValue, pwValue);
 }
 
-function successCreate () {
+function inspectValue( filledId, filledPw) {
+    const regExp = /^[A-Za-z0-9]{4,10}$/;
+    const checkId = regExp.test(filledId);
+    const checkPw = regExp.test(filledPw);
+    console.log(checkId);
+    console.log(checkPw);
+    const checkTrue = checkId && checkPw;
+    if (checkTrue == true) {
+        localStorage.setItem(filledId, filledPw);
+        successCreate();
+    } else if (checkId == false) {
+        alert("아이디는 10줄 이하의 영문과 숫자 조합만 가능합니다");
+    } else alert("비밀번호는 10줄 이하의 영문과 숫자 조합만 가능합니다");
+}
+
+function successCreate() {
     const main = document.querySelector(".container");
     const welcome = document.querySelector(".success");
     main.classList.toggle("hidden");
