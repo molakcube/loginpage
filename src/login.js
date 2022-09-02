942f7b630cb73e79c5a7bee8c6f6bb160748ace1
JunHoe Heo <jhjjang0902@gmail.com>
2022-09-02 23:15:55 +0900

    add password inspect function & revise some logics

--- src/login.js
+++ src/login.js
@@ -3,12 +3,12 @@ const id = document.querySelector(".id input");
 const pw = document.querySelector(".pw input");
 const resetbutton = document.querySelector("#sign-in button");
 
-function checkId (e) {
+function checkId (event) {
     const idValue = id.value;
     const pwValue = pw.value;
     const checkLog = localStorage.getItem(idValue);
+    const stopSubmmit = event.preventDefault();
     const regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-    const stopSubmmit = e.preventDefault();
     
     if (regEmail.test(idValue) !== true) { 	
         alert('형식에 맞지 않는 이메일입니다. 다시 입력해 주세요.');
@@ -19,14 +19,8 @@ function checkId (e) {
         } else if(checkLog !== pwValue) {
             alert("비밀번호를 확인하세요")
         stopSubmmit     
-        } else successLogin ();
+        } else alert("로그인 돼었습니다.");
 }
 
-function successLogin () {
-    const main = document.querySelector(".container");
-    const welcome = document.querySelector(".welcome");
-    main.classList.toggle("hidden");
-    welcome.classList.toggle("hidden");
-}
 
 login.addEventListener("submit",checkId);
--- src/signin.js
+++ src/signin.js
@@ -1,32 +1,42 @@
 const signIn = document.querySelector(".signinForm");
 const id = document.querySelector(".id input");
 const pw = document.querySelector(".pw input");
+const password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{6,20}/g;
 
-function  createId(event) {
-    event.preventDefault();
+function inspectValue(event) {
+    const regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+    const regPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,10}/g;
+
     const idValue = id.value;
     const pwValue = pw.value;
-    inspectValue(idValue, pwValue);
-}
 
-function inspectValue( filledId, filledPw) {
-    const regExp = /^[A-Za-z0-9]{4,10}$/;
-    const checkId = regExp.test(filledId);
-    const checkPw = regExp.test(filledPw);
+    const checkId = regEmail.test(idValue);
+    const checkPw = regPassword.test(pwValue);
     const checkTrue = checkId && checkPw;
+
+    const stopSubmmit = event.preventDefault();
+
     if (checkTrue == true) {
-        localStorage.setItem(filledId, filledPw);
-        successCreate();
+        successCreate(idValue, pwValue);
     } else if (checkId == false) {
-        alert("아이디는 10줄 이하의 영문과 숫자 조합만 가능합니다");
-    } else alert("비밀번호는 10줄 이하의 영문과 숫자 조합만 가능합니다");
+        alert("아이디는 이메일 형식만 가능합니다.");
+        stopSubmmit
+    } else 
+        alert("비밀번호는 6단어 이상, 20단어 이하의 대문자,숫자,특수문자를 포함해야 합니다.");
+        stopSubmmit
 }
 
-function successCreate() {
-    const main = document.querySelector(".container");
-    const welcome = document.querySelector(".success");
-    main.classList.toggle("hidden");
-    welcome.classList.toggle("hidden");
+function successCreate(id, pw) {
+    const userInformation = [];
+    const users = {
+        id: id,
+        pw: pw
+    }
+
+    userInformation.push(users);
+    localStorage.setItem("users",JSON.stringify(userInformation));
+    alert("회원가입 되었습니다.");
+    location.href="login.html";
 }
 
-signIn.addEventListener("submit",createId);
+signIn.addEventListener("submit",inspectValue);
